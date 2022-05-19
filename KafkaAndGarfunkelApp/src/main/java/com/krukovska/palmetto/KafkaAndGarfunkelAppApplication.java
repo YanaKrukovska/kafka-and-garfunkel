516eba3b87ef85d205d88181016f93128acd6592
YanaKrukovska <jana.krua@gmail.com>
2022-05-19 21:40:21 +0300

    Renaming

--- KafkaAndGarfunkelApp/src/main/java/com/krukovska/palmetto/KafkaAndGarfunkelAppApplication.java
+++ KafkaAndGarfunkelApp/src/main/java/com/krukovska/palmetto/KafkaAndGarfunkelAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PalmettoAppApplication {
+public class KafkaAndGarfunkelAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PalmettoAppApplication.class, args);
+        SpringApplication.run(KafkaAndGarfunkelAppApplication.class, args);
     }
 
 }
